5f5b3b20361b86832fb08c4c87d82a264bec969b
WJ-Boshoff <wjbosdev@gmail.com>
2025-01-01 18:22:06 +0200

    Created Custom response Dto

--- src/main/java/com/wjbos/gatesystem/dto/ResponseDto.java
+++ src/main/java/com/wjbos/gatesystem/dto/ResponseDto.java
@@ -1,20 +1,12 @@
 package com.wjbos.gatesystem.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 @AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class ResponseDto {
-
-    private String tokenValue;
-    private LocalDateTime timeIssued;
-    private boolean hasEntered;
-    private boolean hasExited;
+    private String message;
+    private String statusCode;
 }
