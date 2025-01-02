a7983477a70f7af429fc7db8dce9746ea06fe3f3
WJ-Boshoff <wjbosdev@gmail.com>
2025-01-02 18:03:03 +0200

    Custom methods to find tokens

--- src/main/java/com/wjbos/gatesystem/repo/TokenRepository.java
+++ src/main/java/com/wjbos/gatesystem/repo/TokenRepository.java
@@ -3,5 +3,10 @@ package com.wjbos.gatesystem.repo;
 import com.wjbos.gatesystem.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TokenRepository extends JpaRepository<Token, String> {
+    Optional<Token> findByRecipientCellNumber(String cellNumber);
+
+    Optional<Token> findByTokenValue(String tokenValue);
 }
