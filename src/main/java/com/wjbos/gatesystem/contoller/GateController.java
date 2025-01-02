c1426af66857675692b6b9852115ff80d1bd7f00
WJ-Boshoff <wjbosdev@gmail.com>
2025-01-02 18:01:43 +0200

    create a token from controller

--- src/main/java/com/wjbos/gatesystem/contoller/GateController.java
+++ src/main/java/com/wjbos/gatesystem/contoller/GateController.java
@@ -1,6 +1,10 @@
 package com.wjbos.gatesystem.contoller;
 
+import com.wjbos.gatesystem.dto.ResponseDto;
+import com.wjbos.gatesystem.dto.TokenDto;
+import com.wjbos.gatesystem.service.impl.TokenServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
-public class GateController {
+public class TokenController {
+
+    TokenServiceImpl tokenService;
 
     @PostMapping
-    public ResponseEntity<ResponseDto> createEntryToken(String cellNumber){
-
+    public ResponseEntity<TokenDto> createAndSendEntryToken(String cellNumber){
+        tokenService.createToken(cellNumber);
+        tokenService.sendCodeViaSMS();
     }
 }
