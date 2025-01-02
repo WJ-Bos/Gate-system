80aea8998407f8b1a529de6e2725d6b6b016f9cf
WJ-Boshoff <wjbosdev@gmail.com>
2025-01-02 18:01:56 +0200

    Handle custom Exceptions

---
+++ src/main/java/com/wjbos/gatesystem/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.wjbos.gatesystem.exceptions;
+
+import com.wjbos.gatesystem.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception, WebRequest request){
+
+        ErrorResponseDto responseDto = new ErrorResponseDto(
+                request.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(responseDto,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(TokenDoesNotExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleTokenDoesNotExistException(Exception exception, WebRequest request){
+        ErrorResponseDto responseDto = new ErrorResponseDto(
+                request.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(responseDto,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TokenDoesNotExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleTokenValueDoesNotExistException(Exception exception, WebRequest request){
+        ErrorResponseDto responseDto = new ErrorResponseDto(
+                request.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(responseDto,HttpStatus.NOT_FOUND);
+    }
+}
