ce004f6d6e2870aee74a2ab790ff06064af651ef
WJ-Boshoff <wjbosdev@gmail.com>
2025-01-02 17:13:17 +0200

    refactor user table

--- src/main/java/com/wjbos/gatesystem/entity/User.java
+++ src/main/java/com/wjbos/gatesystem/entity/User.java
@@ -1,16 +1,26 @@
 package com.wjbos.gatesystem.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.FutureOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
-    private String userName;
-    private String password;
+
     @Id
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "cell_number")
     private String cellNumber;
 }
