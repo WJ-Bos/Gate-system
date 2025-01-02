7439973cf0278e441cc9a7328f4e09750fa43e2d
WJ-Boshoff <wjbosdev@gmail.com>
2025-01-02 18:02:45 +0200

    Mapping token to dto

--- src/main/java/com/wjbos/gatesystem/mapper/TokenMapper.java
+++ src/main/java/com/wjbos/gatesystem/mapper/TokenMapper.java
@@ -1,4 +1,18 @@
 package com.wjbos.gatesystem.mapper;
 
+import com.wjbos.gatesystem.dto.TokenDto;
+import com.wjbos.gatesystem.entity.Token;
+
 public class TokenMapper {
+
+    public static TokenDto mapToTokenDto(Token token) {
+        TokenDto tokenDto = new TokenDto();
+        tokenDto.setRecipientCellNumber(token.getRecipientCellNumber());
+        tokenDto.setTokenValue(token.getTokenValue());
+        tokenDto.setTokenId(token.getTokenId());
+        tokenDto.setTimeIssued(token.getTimeIssued());
+        tokenDto.setHasEntered(token.isHasEntered());
+        token.setHasExited(token.isHasExited());
+        return tokenDto;
+    }
 }
