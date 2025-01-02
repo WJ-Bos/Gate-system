0ec101e70a688db489498880447f416ed2a77399
WJ-Boshoff <wjbosdev@gmail.com>
2025-01-02 18:03:36 +0200

    fixed user entity

--- src/main/java/com/wjbos/gatesystem/entity/User.java
+++ src/main/java/com/wjbos/gatesystem/entity/User.java
@@ -4,13 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.FutureOrPresent;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
 
