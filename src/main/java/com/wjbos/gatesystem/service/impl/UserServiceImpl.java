d62b8242aef9cefda9843759bed5175ffc225bf6
WJbos <wjbosdev@gmail.com>
2025-01-03 18:06:30 +0200

    Added Basic Sign in logic

--- src/main/java/com/wjbos/gatesystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/wjbos/gatesystem/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.wjbos.gatesystem.service.impl;
 
 import com.wjbos.gatesystem.dto.UserDto;
 import com.wjbos.gatesystem.entity.User;
+import com.wjbos.gatesystem.exceptions.UserDoesNotExistException;
 import com.wjbos.gatesystem.repo.UserRepository;
 import com.wjbos.gatesystem.service.UserService;
 import lombok.AllArgsConstructor;
@@ -23,4 +24,22 @@ public class UserServiceImpl implements UserService {
         newUser.setCellNumber(String.valueOf(user.getCellNumber()));
         userRepository.save(newUser);
     }
+
+    @Override
+    public UserDto signIn(String userName, String password) {
+        User user = userRepository.findByUserName(userName).orElseThrow(
+                () -> new UserDoesNotExistException(userName));
+
+        if(user.getPassword().equals(password)) {
+            UserDto userDto = new UserDto();
+            userDto.setUserName(user.getUserName());
+            userDto.setPassword(user.getPassword());
+            userDto.setCellNumber(String.valueOf(user.getCellNumber()));
+            return userDto;
+        }
+        else return null;
+    }
 }
+
+
+
