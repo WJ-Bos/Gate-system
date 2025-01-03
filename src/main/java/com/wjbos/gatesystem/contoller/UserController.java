f65be518dd9c47528b7996bbdce87f705374b067
WJbos <wjbosdev@gmail.com>
2025-01-03 18:11:40 +0200

    Added basic sign in endpoint (finish)

--- src/main/java/com/wjbos/gatesystem/contoller/UserController.java
+++ src/main/java/com/wjbos/gatesystem/contoller/UserController.java
@@ -4,14 +4,13 @@ package com.wjbos.gatesystem.contoller;
 import com.wjbos.gatesystem.dto.ResponseDto;
 import com.wjbos.gatesystem.dto.UserDto;
 import com.wjbos.gatesystem.entity.User;
+import com.wjbos.gatesystem.repo.UserRepository;
 import com.wjbos.gatesystem.service.impl.UserServiceImpl;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +24,15 @@ public class UserController {
         userService.createUser(user);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(new ResponseDto("Token Created and Sent", "201"));
+                .body(new ResponseDto("User Created", "201"));
+    }
+
+    @GetMapping("/sign-in")
+    public ResponseEntity<ResponseDto> signIn(String userName,
+                                              String password){
+        UserDto userDto = userService.signIn(userName,password);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto("Successful request", "200"));
     }
 }
