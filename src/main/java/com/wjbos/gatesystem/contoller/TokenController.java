d475ee2df66dd0777e3c66bde10eb943a11babed
WJbos <wjbosdev@gmail.com>
2025-01-03 16:51:54 +0200

    updated token endpoint url

--- src/main/java/com/wjbos/gatesystem/contoller/TokenController.java
+++ src/main/java/com/wjbos/gatesystem/contoller/TokenController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/token")
 public class TokenController {
 
     TokenServiceImpl tokenService;
