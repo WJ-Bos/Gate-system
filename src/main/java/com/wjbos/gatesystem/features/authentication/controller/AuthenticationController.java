4aea67ab44cdea96ba8f67232d1de32661487625
WJbos <wjbosdev@gmail.com>
2025-01-04 15:32:28 +0200

    Refactor AuthenticationController to return user details.
    Replaced the hardcoded response in `getUser` with a call to `AuthenticationServiceImpl` to fetch user details. This improves functionality by dynamically fetching

--- src/main/java/com/wjbos/gatesystem/features/authentication/controller/AuthenticationController.java
+++ src/main/java/com/wjbos/gatesystem/features/authentication/controller/AuthenticationController.java
@@ -1,13 +1,17 @@
 package com.wjbos.gatesystem.features.authentication.controller;
 
+import com.wjbos.gatesystem.features.authentication.entity.AuthenticationUser;
+import com.wjbos.gatesystem.features.authentication.service.impl.AuthenticationServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api/v1/authentication")
 public class AuthenticationController {
 
+    AuthenticationServiceImpl authService;
+
     @GetMapping("user")
-    public String getUser(){
-        return "Hello User";
+    public AuthenticationUser getUser(){
+        return authService.getUser("user@example.com");
     }
 }
