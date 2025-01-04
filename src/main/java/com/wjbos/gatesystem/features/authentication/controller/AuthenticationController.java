4ce020e32eb6d5f77690322bb0685ebafe67cfd3
WJbos <wjbosdev@gmail.com>
2025-01-04 15:20:09 +0200

    Add a new API endpoint for user authentication info
    Introduced a GET endpoint at "api/v1/authentication/user" that returns a simple response. This provides a basic structure for future enhancements to user authentication functionality.

--- src/main/java/com/wjbos/gatesystem/features/authentication/controller/AuthenticationController.java
+++ src/main/java/com/wjbos/gatesystem/features/authentication/controller/AuthenticationController.java
@@ -1,4 +1,13 @@
 package com.wjbos.gatesystem.features.authentication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/v1/authentication")
 public class AuthenticationController {
+
+    @GetMapping("user")
+    public String getUser(){
+        return "Hello User";
+    }
 }
