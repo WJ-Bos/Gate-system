f9c0f2bfd356c5139493d4423009caaa0fed6959
WJbos <wjbosdev@gmail.com>
2025-01-04 15:26:25 +0200

    Add method to find user by email in repository
    Introduced a `findByEmail` method to `AuthenticationUserRepository` to enable fetching users based on their email addresses. This change improves flexibility and supports email-based user lookups.

--- src/main/java/com/wjbos/gatesystem/features/authentication/repository/AuthenticationUserRepository.java
+++ src/main/java/com/wjbos/gatesystem/features/authentication/repository/AuthenticationUserRepository.java
@@ -4,6 +4,9 @@ import com.wjbos.gatesystem.features.authentication.entity.AuthenticationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthenticationUserRepository extends JpaRepository<AuthenticationUser, Long> {
+    Optional<AuthenticationUser> findByEmail(String email);
 }
