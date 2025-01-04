ff933dd897cd931180c4631258bcc1855f37fe95
WJbos <wjbosdev@gmail.com>
2025-01-04 14:20:23 +0200

    removed  User Service for refactor

--- src/main/java/com/wjbos/gatesystem/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.wjbos.gatesystem.service;
-
-import com.wjbos.gatesystem.dto.UserDto;
-
-public interface UserService {
-    void createUser(UserDto user);
-
-    UserDto signIn(String userName, String password);
-}
