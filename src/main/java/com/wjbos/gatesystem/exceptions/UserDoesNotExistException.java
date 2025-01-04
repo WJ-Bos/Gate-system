830770537fcacc99f09198742e4bfcd9eb87ec22
WJbos <wjbosdev@gmail.com>
2025-01-04 14:20:06 +0200

    removed  User Exception for refactor

--- src/main/java/com/wjbos/gatesystem/exceptions/UserDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.wjbos.gatesystem.exceptions;
-
-public class UserDoesNotExistException extends RuntimeException {
-    public UserDoesNotExistException(String username) {
-        super(String.format("User with username: %s does not exist"));
-    }
-}
