a0d2d1ca13eca6c12b9912a66e0b13c8115534f9
WJbos <wjbosdev@gmail.com>
2025-01-04 15:15:27 +0200

    Add initial database seeding configuration
    Introduce a new configuration class, `LoadDatabaseConfig`, to seed the database with an initial `AuthenticationUser`. This ensures a default user is available for authentication during application startup.

---
+++ src/main/java/com/wjbos/gatesystem/config/LoadDatabaseConfig.java
@@ -0,0 +1,19 @@
+package com.wjbos.gatesystem.config;
+
+import com.wjbos.gatesystem.features.authentication.entity.AuthenticationUser;
+import com.wjbos.gatesystem.features.authentication.repository.AuthenticationUserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabaseConfig {
+
+    @Bean
+    public CommandLineRunner initDatabase(AuthenticationUserRepository authenticationUserRepository){
+        return args -> {
+            AuthenticationUser authenticationUser = new AuthenticationUser(1L,"user@example.com","password");
+            authenticationUserRepository.save(authenticationUser);
+        };
+    }
+}
